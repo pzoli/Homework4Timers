d3696051e390a8662aaadfbc2139afab8833a9a8
Papp Zoltan <papp.zoltan@infokristaly.hu>
2026-09-23 16:21:37 +0200

    Initial Commit

---
+++ Homework4Timers/Homework4TimersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Homework4TimersApp.swift
+//  Homework4Timers
+//
+//  Created by Papp Zoltán on 2026. 09. 23..
+//
+
+import SwiftUI
+
+@main
+struct Homework4TimersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
